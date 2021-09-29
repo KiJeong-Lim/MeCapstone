554312a8328a243088438333ffc7d585eecb85a8
KiJeong-Lim <gijungdduk@naver.com>
2021-09-30 07:00:13 +0900

    .

--- third/third.h
+++ third/third.h
@@ -11,7 +11,6 @@ typedef double mA_t;
 typedef double V_t;
 typedef double Ohm_t;
 typedef unsigned int long Time_t;
-enum State_t { BAD_STATE = 0, CC_STATE, CV_STATE, FINISH_STATE };
 
 // Macros
 
@@ -26,29 +25,5 @@ enum State_t { BAD_STATE = 0, CC_STATE, CV_STATE, FINISH_STATE };
 #define CHARGING_COEFFICIENT        0.5
 #define CUTOFF_AMPERE               2000.0
 #define CUTOFF_VOLTAGE              5.0
-#define VOLTAGE_OUTPUT_AT_255       6.0
-
-// Declarations
-
-/* Core */
-V_t findVoltage(mA_t wanted_current);
-void analyzeCell();
-
-/* Setters */
-void setState(State_t new_state);
-void setPWM(V_t wanted_voltage);
-
-/* Readers */
-double readAnalogSignal(uint8_t pin_num, Time_t duration);
-void readSensorFor100ms();
-
-/* Printers */
-void greeting();
-void goodbye();
-void showMeasuredValues();
-
-/* Initializers */
-void initializePins();
-void initializeLCD(int row_dim, int col_dim);
 
 #endif
