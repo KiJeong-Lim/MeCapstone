bab8c521a81eaebe78c76062a32e98887fa3b402
KiJeong-Lim <gijungdduk@naver.com>
2021-09-30 18:58:12 +0900

    .

---
+++ third/table.cpp
@@ -0,0 +1,6 @@
+#include "third.h"
+
+V_t lookupOcvTable(Time_t const run_time)
+{
+
+}
--- third/third.h
+++ third/third.h
@@ -11,6 +11,7 @@ typedef double mA_t;
 typedef double V_t;
 typedef double Ohm_t;
 typedef unsigned int long Time_t;
+enum State_t { BAD_STATE = 0, CC_STATE, CV_STATE, FINISH_STATE };
 
 // Macros
 
@@ -25,5 +26,32 @@ typedef unsigned int long Time_t;
 #define CHARGING_COEFFICIENT        0.5
 #define CUTOFF_AMPERE               2000.0
 #define CUTOFF_VOLTAGE              5.0
+#define VOLTAGE_OUTPUT_AT_255       6.0
+
+// Declarations
+
+/* "table.cpp" */
+V_t lookupOcvTable(Time_t run_time);
+
+/* Core */
+V_t findVoltage(mA_t wanted_current);
+void analyzeCell();
+
+/* Setters */
+void setState(State_t new_state);
+void setPWM(V_t wanted_voltage);
+
+/* Readers */
+double readAnalogSignal(uint8_t pin_num, Time_t duration);
+void readSensorFor100ms();
+
+/* Printers */
+void greeting();
+void goodbye();
+void showMeasuredValues();
+
+/* Initializers */
+void initializePins();
+void initializeLCD(int row_dim, int col_dim);
 
 #endif
