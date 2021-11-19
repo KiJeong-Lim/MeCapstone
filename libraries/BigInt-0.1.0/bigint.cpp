0cdb21bec0693b9ec92cd374d3b25f2439b9544f
KiJeong-Lim <gijungdduk@naver.com>
2021-11-19 15:08:49 +0900

    .

---
+++ libraries/BigInt-0.1.0/bigint.cpp
@@ -0,0 +1,7 @@
+/* Name:      BigInt
+** Synopsis:  Library for big integers
+** Author:    Ki-jeong Lim
+** Version:   0.1.0
+*/
+
+#include "bigint.h"
---
+++ libraries/BigInt-0.1.0/bigint.h
@@ -0,0 +1,13 @@
+/* Name:      BigInt
+** Synopsis:  Library for big integers
+** Author:    Ki-jeong Lim
+** Version:   0.1.0
+*/
+
+#ifndef BIGINT
+#define BIGINT
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#endif
